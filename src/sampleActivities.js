12c4395a7939f3b435060ce701fd9002c6be2d8f
Matthew Knudsen <mknudsen01@gmail.com>
2017-02-26 16:39:12 -0800

    Add some other dates into sample activities

--- src/sampleActivities.js
+++ src/sampleActivities.js
@@ -1,18 +1,17 @@
 const activitiesByTimestamp = {
-  // activityTimestamps: [1488058736421, 1488058788421, 1488058446421, 1488051326421, 1488051326555]
   1488058736421: {
     text: "hang at Golden Gate park 🍺",
   },
   1488078736421: {
     text: "soak up some 🌞 at Dolores park",
   },
-  1488058788421: {
+  1487558788421: {
     text: "climb at Planet Granite -- 3 hours",
   },
   1488058446421: {
     text: "run 4.5 miles 🏃",
   },
-  1488059646421: {
+  1486959646421: {
     text: "surfing 🏄 with Salar in Santa Cruz",
   },
   1488051326421: {
