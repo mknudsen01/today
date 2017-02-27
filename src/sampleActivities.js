c53515b6a23b5650f874646f45be5a27e1e60cde
Matthew Knudsen <mknudsen01@gmail.com>
2017-02-26 16:28:11 -0800

    Add sample activitiesByTimestamp file

---
+++ src/sampleActivities.js
@@ -0,0 +1,26 @@
+const activitiesByTimestamp = {
+  // activityTimestamps: [1488058736421, 1488058788421, 1488058446421, 1488051326421, 1488051326555]
+  1488058736421: {
+    text: "hang at Golden Gate park 🍺",
+  },
+  1488078736421: {
+    text: "soak up some 🌞 at Dolores park",
+  },
+  1488058788421: {
+    text: "climb at Planet Granite -- 3 hours",
+  },
+  1488058446421: {
+    text: "run 4.5 miles 🏃",
+  },
+  1488059646421: {
+    text: "surfing 🏄 with Salar in Santa Cruz",
+  },
+  1488051326421: {
+    text: "read Infinite Jest -- 40 pages",
+  },
+  1488051326555: {
+    text: "read Code by Petzold -- 90 pages",
+  }
+}
+
+export default activitiesByTimestamp;
