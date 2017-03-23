786ff41eb0c7e26ea396e4cde7ec3d3f9155f63f
Matthew Knudsen <mknudsen01@gmail.com>
2017-03-23 15:44:10 -0700

    Add escape listener to main activity input

--- src/components/AddActivityForm.js
+++ src/components/AddActivityForm.js
@@ -3,6 +3,21 @@ import React, { Component, PropTypes } from 'react';
 import { getDescriptionAndTags } from '../helpers';
 
 class AddActivityForm extends Component {
+
+  componentDidMount() {
+    this.description.addEventListener('keydown', this.escapeListener.bind(this));
+  }
+
+  componentWillUnmount() {
+    this.description.removeEventListener('keydown', this.escapeListener)
+  }
+
+  escapeListener(e) {
+    if (e.keyCode === 27) {
+      this.description.blur();
+    }
+  }
+
   createActivity(e) {
     e.preventDefault();
     const { description, tags } = getDescriptionAndTags(this.description.value);
@@ -29,6 +44,7 @@ class AddActivityForm extends Component {
           ref={(node) => this.description = node}
           placeholder="went to the park"
           className="pv- ph- col--9"
+          autoFocus
         />
         <div
           onClick={(e) => this.createActivity(e)}
