ba993660eb7c6fd2af2ae8dc28a768d4637c780f
Matthew Knudsen <mknudsen01@gmail.com>
2017-03-28 08:57:10 -0700

    Adjust some styling of login/sign up forms

--- src/components/Login.js
+++ src/components/Login.js
@@ -27,51 +27,73 @@ class Login extends Component {
 
   render() {
     return (
-      <div className="container--full">
+      <div className="container--full bg--wet-asphalt">
         <div className="container">
           <div className="row row--center">
-            <form
-              onSubmit={this.createUser}
-              ref={(node) => this.createUserForm = node}
-            >
-              <input
-                type="email"
-                required
-                ref={(node) => this.createUserEmail = node}
-                placeholder="email"
-                className="pv- ph-"
-              />
-              <input
-                type="password"
-                required
-                ref={(node) => this.createUserPassword = node}
-                placeholder="password"
-                className="pv- ph-"
-              />
-              <button type="submit">Create User</button>
-            </form>
-          </div>
-          <div className="row row--center mt+">
-            <form
-              onSubmit={this.loginUser}
-              ref={(node) => this.loginForm = node}
-            >
-              <input
-                type="email"
-                required
-                ref={(node) => this.loginEmail = node}
-                placeholder="email"
-                className="pv- ph-"
-              />
-              <input
-                type="password"
-                required
-                ref={(node) => this.loginPassword = node}
-                placeholder="password"
-                className="pv- ph-"
-              />
-              <button type="submit">Log in</button>
-            </form>
+            <div className="col--6">
+              <div className="bg--clouds mt++ overflow-x--hidden box-shadow--5">
+                <div className="row mv+ pv">
+                  <form
+                    className="col--12"
+                    onSubmit={this.createUser}
+                    ref={(node) => this.createUserForm = node}
+                  >
+                    <input
+                      type="email"
+                      required
+                      ref={(node) => this.createUserEmail = node}
+                      placeholder="email"
+                      className="pv- ph- col--10"
+                    />
+                    <input
+                      type="password"
+                      required
+                      ref={(node) => this.createUserPassword = node}
+                      placeholder="password"
+                      className="pv- ph- col--10 mv-"
+                    />
+                    <div className="row row--center">
+                      <div
+                        className="pv-- ph- bg--peter-river bg--belize-hole--hover text--white transition--3-10 pointer col--3 flex align-items--center justify-content--center"
+                        onClick={this.createUser}
+                      >
+                        Create User
+                      </div>
+                    </div>
+                  </form>
+                </div>
+                <div className="row row--center mv+">
+                  <form
+                    onSubmit={this.loginUser}
+                    ref={(node) => this.loginForm = node}
+                    className="col--12"
+                  >
+                    <input
+                      type="email"
+                      required
+                      ref={(node) => this.loginEmail = node}
+                      placeholder="email"
+                      className="pv- ph- col--10"
+                    />
+                    <input
+                      type="password"
+                      required
+                      ref={(node) => this.loginPassword = node}
+                      placeholder="password"
+                      className="pv- ph- col--10 mv-"
+                    />
+                    <div className="row row--center">
+                      <div
+                        className="pv-- ph- bg--peter-river bg--belize-hole--hover text--white transition--3-10 pointer col--3 flex align-items--center justify-content--center"
+                        onClick={this.loginUser}
+                      >
+                        Log in
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
