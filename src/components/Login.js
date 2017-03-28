808094541db90b3fedb5eca48ba0c2328297d1fa
Matthew Knudsen <mknudsen01@gmail.com>
2017-03-28 09:09:33 -0700

    Switch between login/create user

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,10 @@ class Login extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      isLogin: false,
+    }
+
     this.loginUser = this.loginUser.bind(this);
     this.createUser = this.createUser.bind(this);
   }
@@ -25,7 +29,93 @@ class Login extends Component {
     this.createUserForm.reset();
   }
 
+  toggleLogin(value) {
+    this.setState({
+      isLogin: value,
+    })
+  }
+
   render() {
+    const { isLogin } = this.state;
+    const createUserForm = (
+      <form
+        className="col--12"
+        onSubmit={this.createUser}
+        ref={(node) => this.createUserForm = node}
+      >
+        <input
+          type="email"
+          required
+          ref={(node) => this.createUserEmail = node}
+          placeholder="email"
+          className="pv- ph- col--10"
+        />
+        <input
+          type="password"
+          required
+          ref={(node) => this.createUserPassword = node}
+          placeholder="password"
+          className="pv- ph- col--10 mv-"
+        />
+        <div className="row row--center">
+          <div
+            className="pv-- ph- bg--peter-river bg--belize-hole--hover text--white transition--3-10 pointer col--3 flex align-items--center justify-content--center"
+            onClick={this.createUser}
+          >
+            Create User
+          </div>
+        </div>
+        <div className="row row--center mt-">
+          <div
+            className="col--4 font--12 pointer"
+            onClick={() => this.toggleLogin(true)}
+          >
+            Already a user? Log in
+          </div>
+        </div>
+      </form>
+    );
+
+    const loginForm = (
+      <form
+        onSubmit={this.loginUser}
+        ref={(node) => this.loginForm = node}
+        className="col--12"
+      >
+        <input
+          type="email"
+          required
+          ref={(node) => this.loginEmail = node}
+          placeholder="email"
+          className="pv- ph- col--10"
+        />
+        <input
+          type="password"
+          required
+          ref={(node) => this.loginPassword = node}
+          placeholder="password"
+          className="pv- ph- col--10 mv-"
+        />
+        <div className="row row--center">
+          <div
+            className="pv-- ph- bg--peter-river bg--belize-hole--hover text--white transition--3-10 pointer col--3 flex align-items--center justify-content--center"
+            onClick={this.loginUser}
+          >
+            Log in
+          </div>
+        </div>
+        <div className="row row--center mt-">
+          <div
+            className="col--4 font--12 pointer"
+            onClick={() => this.toggleLogin(false)}
+          >
+            Not a user? Create an account
+          </div>
+        </div>
+      </form>
+    );
+
+
     return (
       <div className="container--full bg--wet-asphalt">
         <div className="container">
@@ -33,64 +123,12 @@ class Login extends Component {
             <div className="col--6">
               <div className="bg--clouds mt++ overflow-x--hidden box-shadow--5">
                 <div className="row mv+ pv">
-                  <form
-                    className="col--12"
-                    onSubmit={this.createUser}
-                    ref={(node) => this.createUserForm = node}
-                  >
-                    <input
-                      type="email"
-                      required
-                      ref={(node) => this.createUserEmail = node}
-                      placeholder="email"
-                      className="pv- ph- col--10"
-                    />
-                    <input
-                      type="password"
-                      required
-                      ref={(node) => this.createUserPassword = node}
-                      placeholder="password"
-                      className="pv- ph- col--10 mv-"
-                    />
-                    <div className="row row--center">
-                      <div
-                        className="pv-- ph- bg--peter-river bg--belize-hole--hover text--white transition--3-10 pointer col--3 flex align-items--center justify-content--center"
-                        onClick={this.createUser}
-                      >
-                        Create User
-                      </div>
-                    </div>
-                  </form>
-                </div>
-                <div className="row row--center mv+">
-                  <form
-                    onSubmit={this.loginUser}
-                    ref={(node) => this.loginForm = node}
-                    className="col--12"
-                  >
-                    <input
-                      type="email"
-                      required
-                      ref={(node) => this.loginEmail = node}
-                      placeholder="email"
-                      className="pv- ph- col--10"
-                    />
-                    <input
-                      type="password"
-                      required
-                      ref={(node) => this.loginPassword = node}
-                      placeholder="password"
-                      className="pv- ph- col--10 mv-"
-                    />
-                    <div className="row row--center">
-                      <div
-                        className="pv-- ph- bg--peter-river bg--belize-hole--hover text--white transition--3-10 pointer col--3 flex align-items--center justify-content--center"
-                        onClick={this.loginUser}
-                      >
-                        Log in
-                      </div>
-                    </div>
-                  </form>
+                  {
+                    isLogin && loginForm
+                  }
+                  {
+                    !isLogin && createUserForm
+                  }
                 </div>
               </div>
             </div>
