f375f18def97a3a0fa5737485484e0f89f45d844
Matthew Knudsen <mknudsen01@gmail.com>
2017-04-20 13:20:10 -0700

    Remove buttons/links on activity description

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -2,6 +2,8 @@ import React, { Component, PropTypes } from 'react';
 import moment from 'moment';
 
 import TagList from './TagList';
+import Row from './layout/Row';
+import Column from './layout/Column';
 
 class Activity extends Component {
 
@@ -11,45 +13,18 @@ class Activity extends Component {
 
     return (
       <div>
-        <div className="row row--middle row--start">
-          <div className="col--auto flex align-items--center">
-            <div
-              className="flex cursor--pointer"
-              onClick={isEditing ?
-                () => cancelEdit() :
-                () => editActivity(timestamp)
-              }
-            >
-              {description}
-            </div>
-            {
-              isEditing && (
-                <span
-                  className="flex cursor--pointer font--12 ml p- color--blue"
-                  onClick={() => cancelEdit()}
-                >
-                  cancel
-                </span>
-              )
+        <Row middle start>
+          <Column
+            span={12}
+            className="cursor--pointer p- bg--clouds"
+            onClick={isEditing ?
+              () => cancelEdit() :
+              () => editActivity(timestamp)
             }
-            {
-              !isEditing && (
-                <span
-                  className="flex cursor--pointer font--12 ml p- color--blue"
-                  onClick={() => editActivity(timestamp)}
-                >
-                  edit
-                </span>
-              )
-            }
-            <span
-              className="flex cursor--pointer ml-- p- color-red"
-              onClick={() => deleteActivity(timestamp)}
-            >
-              &times;
-            </span>
-          </div>
-        </div>
+          >
+            {description}
+          </Column>
+        </Row>
         <TagList
           tags={tags}
         />
