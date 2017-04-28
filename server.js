763f1442cd583bb37254d55d81c9e62425bb03f7
Matthew Knudsen <mknudsen01@gmail.com>
2017-04-27 17:17:59 -0700

    Add bare-bones server
    * serves up client/index.html
    * returns json in single /test endpoint

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('port', (process.env.PORT || 3001));
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+
+app.get('/test', function(req, res) {
+  console.log('==================')
+  console.log('requesting /test');
+  console.log('==================')
+  res.json({
+    'a': 'this is the result of /test'
+  });
+});
+
+app.get('*', function (req, res) {
+  console.log('made a request, any old request');
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+});
